01d8234ab6f53564948b151b2236aaa259b78458
CastellucciLuca <castellucciluca98@gmail.com>
2023-01-26 16:31:13 +0100

    Add checkbox to make user customize password

--- index.php
+++ index.php
@@ -1,9 +1,26 @@
 <?php
 session_start();
-include_once __DIR__ . './functions.php';
+include_once __DIR__ . '/functions.php';
 
-$passwordCharacters = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz\!$%&/()=?^*+-=';
+$passwordCharacters = '';
 $length = $_GET['passwordLenght'];
+
+$Onlyletters = $_GET['letters'];
+$Onlynumbers = $_GET['numbers'];
+$Onlyspecials = $_GET['specials'];
+
+//USER SELECTION
+if ($Onlyletters == 'true') {
+    $passwordCharacters .= 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+}
+
+if ($Onlynumbers == 'true') {
+    $passwordCharacters .= '0123456789';
+}
+
+if ($Onlyspecials == 'true') {
+    $passwordCharacters .= '|\!$%&/()=?^*+-=';
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -15,9 +32,21 @@ $length = $_GET['passwordLenght'];
 </head>
 <body>
     <header>
-        <form action="./index.php" method="GET">
-            <label for="password-lenght">Inserisci lunghezza password</label>
-            <input type="number" name="passwordLenght" id="password-lenght">
+    <form action="./index.php" method="GET">
+            <div class="characterLength">
+                <label for="password-lenght">Insert password lenght</label>
+                <input type="number" name="passwordLenght" id="password-lenght">
+            </div>
+            <div class="choose-characters-type">
+                <input type="checkbox" id="letters" name="letters" value="true">
+                <label for="letters">Letters</label>
+                <br>
+                <input type="checkbox" id="numbers" name="numbers" value="true">
+                <label for="numbers">Numbers</label>
+                <br>
+                <input type="checkbox" id="specials" name="specials" value="true">
+                <label for="specials">Specials characters</label>
+            </div>
             <button type="submit">Invia</button>
         </form>
     </header>

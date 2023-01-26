4e3c5b379eb47b9ca5f44f88f8485b08a9a05eed
CastellucciLuca <castellucciluca98@gmail.com>
2023-01-26 15:46:32 +0100

    Generating password lenght

--- index.php
+++ index.php
@@ -1,3 +1,22 @@
+<?php
+$passwordCharacters = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz|\!£$%&/()=?^*+-=';
+$length = $_GET['passwordLenght'];
+
+if (isset($length)) {
+
+    function generatePassword($length, $passwordCharacters)
+    {
+        $passwordString = '';
+
+        for ($i = 0; $i < $length; $i++) {
+            $randomNumber = rand(1, strlen($passwordCharacters));
+            $passwordString .= $passwordCharacters[$randomNumber];
+        }
+
+        return $passwordString;
+    }
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,6 +27,14 @@
 </head>
 <body>
     <main>
+        <form action="./index.php" method="GET">
+            <label for="password-lenght">Inserisci lunghezza password</label>
+            <input type="number" name="passwordLenght" id="password-lenght">
+            <button type="submit">Invia</button>
+        </form>
+        <?php
+        echo generatePassword($length, $charactersForPassword);
+        ?>  
     </main>
 </body>
 </html>
